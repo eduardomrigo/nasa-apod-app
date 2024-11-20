c494b95934c69c54c3bd6f12408aff92a2a55b4c
Eduardo Rigo <eduardorigom@gmail.com>
2024-11-20 19:09:57 -0300

    fix: SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,47 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+
 export const metadata: Metadata = {
-  title: "NASA APOD",
-  description: "As",
-};
+  generator: 'Next.js',
+  title: 'NASA Dashboard',
+  description: 'Explore NASA API',
+  applicationName: 'NASA Dashboard',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript', 'typescript', 'nextjs', 'frontend', 'developer', 'front end', 'programmer'],
+  authors: [{ name: 'Eduardo Rigo', url: 'https://eduardev.com' }],
+  creator: 'Eduardo Rigo',
+  publisher: 'Eduardo Rigo',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: 'NASA Dashboard',
+    description: 'Explore NASA API',
+    url: 'https://nasa.eduardev.com',
+    siteName: 'NASA Dashboard',
+    images: [
+      {
+        url: 'https://www.nasa.eduardev.com/images/bg.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://www.nasa.eduardev.com/images/bg-g.png',
+        width: 1800,
+        height: 1600,
+      },
+    ],
+    type: 'website',
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+  },
+}
+
 
 export default function RootLayout({
   children,
